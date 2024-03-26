8956ea3a02a9aa42c3ab526cc112205aaddf7b81
Claire Price <claire.price@mail.com>
2024-03-26 22:06:01 +0000

    handle click selects data

--- src/components/CardPA.jsx
+++ src/components/CardPA.jsx
@@ -2,26 +2,59 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
-function CardPA({ data }) {
 
-    function handleClick() {
-        console.log(data);
-       }
+function CardPA(props) {
 
-    function selectProject() {
-        const [projectData, setProjectData] = useState();
-    }
+    // function selectProject({ data }) {
+        
+
+        
+
+    // }
+
+    const [projectData, setProjectData] = useState(props.data);
+
+    // function handleClick(event) {
+    //     event.preventDefault();
+    //     const selectProject = () => {
+    //         setProjectData(event);
+
+    //     }
+    //     function selectProject() {
+            
+    //     }
+    //     const selectedProject = this.data;
+    //     setProjectData(selectedProject);
+    //     console.log(selectedProject);
+    //     console.log(projectData);
+
+    //    }
+
+    //    
+    
+    // const selectedProject = this.data;
+    //     setProjectData(selectedProject);
+    //     console.log(selectedProject);
 
     
 
+    const handleClick = (e) => {
+        const selectedData = e.target.value;
+        setProjectData(selectedData);
+        console.log(projectData);
+        
+    }
 
+    // const allData = props.data;
+    // console.log(allData);
 
     return (
         <div>
-            <img scr={data.image} />
-            <Link to="/Detail" onClick={handleClick}><h5>{data.title}</h5></Link>
-            <a href={data.linkDeployed} target="_blank" rel="noreferrer noopener">Deployed application</a>
-            <a href={data.linkRepo} target="_blank" rel="noreferrer noopener">GitHub repository</a>
+            <img scr={props.data.image} />
+            <h5>{props.data.title}</h5>
+            <a href={props.data.linkDeployed} target="_blank" rel="noreferrer noopener">Deployed application</a>
+            <a href={props.data.linkRepo} target="_blank" rel="noreferrer noopener">GitHub repository</a>
+            <button type="button" onClick={handleClick}><Link to="/Detail" >View more</Link></button>
         </div>
     );
 }
